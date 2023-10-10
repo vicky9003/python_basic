f6a1f18b1607d95201944750f3bf3f7a7132e368
vicky9003 <tusharmpagar@gmail.com>
2023-10-10 17:11:04 +0530

    added

---
+++ function/function.py
@@ -0,0 +1,14 @@
+def say_hi(name):
+	print('hi,'+ name)
+print("Let's greet the entire world")
+say_hi('world')
+
+
+def add(a,b):
+	return a+b
+result=add(4,8)
+print(result)
+
+def welcome(name='learner',location='this tutorial'):
+	print("hi,",name,"welcome to",location)
+welcome()
---
+++ globalvarible/globalvarible.py
@@ -0,0 +1,4 @@
+x='Demo'
+def myfun():
+	print("This is a "+ x +" project")
+myfun()
